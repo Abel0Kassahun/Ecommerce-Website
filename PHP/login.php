cf94d05c58ba4cdef1ef22597dd1568500025c0a
Abel0Kassahun <104197778+Abel0Kassahun@users.noreply.github.com>
2023-05-25 01:44:51 +0300

    login and signup on js and api (signup.php isn't done yet)

--- PHP/login.php
+++ PHP/login.php
@@ -3,6 +3,10 @@
     $conn = new Connect;
     $connect = $conn -> getConnection();
 
+    header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         // header('Content-Type: application/json');
     
